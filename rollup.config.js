57a69d7f59bb2c73af6a758ec99b367d6245cf38
Fabian Riewe <fabianriewe00@gmail.com>
2021-02-11 10:43:27 +0100

    fix: refactored rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
-import { createFilter } from "@rollup/pluginutils";
 import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript2";
-
-const filter = createFilter(["**/*.gql", "**/*.svg"]);
 
 export default {
   input: "src/index.ts",
@@ -27,14 +24,6 @@ export default {
     }),
     {
       name: "string",
-      transform(code, id) {
-        if (filter(id)) {
-          return {
-            code: `export default ${JSON.stringify(code)};`,
-            map: { mappings: "" },
-          };
-        }
-      },
     },
   ],
 };
